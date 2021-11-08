4473fe0cf19a5d149a432db55006e3bf68a916e5
carlos lopez <carlos25u007@gmail.com>
2021-11-08 18:38:23 -0400

    Haciendo la BLL de proyecto

---
+++ BLL/ProyectosBLL.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_CarlosLopez_20190720.DAL;
+using P2_AP1_CarlosLopez_20190720.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_CarlosLopez_20190720.BLL
+{
+    class ProyectosBLL
+    {
+        public static bool Existe(int id)
+        {
+            bool encontrado = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                encontrado = contexto.Proyectos.Any(e => e.ProyectoId == id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return encontrado;
+        }
+
+        public static bool Insertar(Proyectos proyecto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Proyectos.Add(proyecto);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Proyectos proyecto)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                contexto.Entry(proyecto).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Guardar(Proyectos proyecto)
+        {
+            if (Existe(proyecto.ProyectoId))
+                return Insertar(proyecto);
+            else
+                return Modificar(proyecto);
+        }
+
+        public static Proyectos Buscar(int id)
+        {
+            Contexto contexto = new Contexto();
+            Proyectos proyecto = new Proyectos();
+
+            try
+            {
+                proyecto = contexto.Proyectos.Include(x => x.Detalle).Where(p => p.ProyectoId == id).SingleOrDefault();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return proyecto;
+        }
+
+        public static bool Eliminar(int id)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                var proyecto = contexto.Proyectos.Find(id);
+                contexto.Entry(proyecto).State = EntityState.Deleted;
+
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static List<Proyectos> GetList(Expression<Func<Proyectos, bool>> criterio)
+        {
+            var lista = new List<Proyectos>();
+            Contexto contexto = new Contexto();
+
+            try
+            {
+                lista = contexto.Proyectos.Where(criterio).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+                contexto.Dispose();
+            }
+
+            return lista;
+        }
+    }
+}
